5a121e471394dcc6ab7f0e73beef00fff054c7cf
salvador <salvadorjnt@gmail.com>
2024-05-23 15:15:34 -0600

    Created use-case load users by page

---
+++ src/users/use-cases/load-users-by-page.js
@@ -0,0 +1,15 @@
+import { localhostUserToModel } from "../mappers/localhost-user.mapper";
+
+/**
+ * 
+ * @param {Number} page 
+ * @returns {Promise<User[]>}
+ */
+export const loadUsersByPage = async (page = 1) => {
+    const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const users = data.data.map(localhostUserToModel)
+    return users;
+}
