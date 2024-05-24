94dd36fadd6e8208616cdb473ce52a023c96cc69
salvador <salvadorjnt@gmail.com>
2024-05-23 23:11:13 -0600

    Implemented loadNextPage method

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -1,10 +1,15 @@
+import { loadUsersByPage } from "../use-cases/load-users-by-page";
+
 const state = {
     currentPage: 0,
     users: [],
 }
 
 const loadNextPage = async () => {
-    throw new Error('Not implemented');
+    const users = await loadUsersByPage(state.currentPage+1)
+    if (users.length === 0) return;
+    state.currentPage += 1;
+    state.users = users;
 }
 
 const loadPreviousPage = async () => {
@@ -25,6 +30,14 @@ export default {
     onUserChanged,
     reloadPage,
 
+    /**
+     * 
+     * @returns {User[]}
+     */
     getUsers: () => [...state.users],
+    /**
+     * 
+     * @returns Number
+     */
     getCurrentPage: () => state.currentPage,
 }
