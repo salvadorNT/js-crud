28bc5516b117b89586f64388bff746c55fbfc986
salvador <salvadorjnt@gmail.com>
2024-05-23 23:13:51 -0600

    Added functionality to render-table

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,6 +1,9 @@
+import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store";
 
 export const UserApp = async(element) => {
     element.innerHTML = '...Loading';
     await usersStore.loadNextPage();
+    element.innerHTML = '';
+    renderTable(element);
 }
