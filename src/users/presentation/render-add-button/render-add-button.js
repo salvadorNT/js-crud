02c82938e50c74b8a51ec3b3c4c5c0a4c598a9f7
salvador <salvadorjnt@gmail.com>
2024-05-24 21:41:26 -0600

    Created add user button

---
+++ src/users/presentation/render-add-button/render-add-button.js
@@ -0,0 +1,16 @@
+import './render-add-button.css';
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const renderAddButton = (element) => {
+    const fabButton = document.createElement('button');
+    fabButton.innerText = '+';
+    fabButton.classList.add('fab-button');
+
+    element.append(fabButton);
+
+    fabButton.addEventListener('click', () => {
+        throw Error('Not implemented.')
+    })
+}
