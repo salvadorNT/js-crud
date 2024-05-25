af32b0a41baa89094c817decca3b969ab93d04d9
salvador <salvadorjnt@gmail.com>
2024-05-24 21:17:11 -0600

    render pagination buttons from users table

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,3 +1,4 @@
+import { renderButtons } from "./presentation/render-buttons/render-buttons";
 import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store";
 
@@ -6,4 +7,5 @@ export const UserApp = async(element) => {
     await usersStore.loadNextPage();
     element.innerHTML = '';
     renderTable(element);
+    renderButtons(element);
 }
